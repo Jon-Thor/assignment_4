429d5bbb7c3566ad480752b4bc76873821116e87
Jon-Thor <101129693+Jon-Thor@users.noreply.github.com>
2022-03-18 12:03:16 +0000

    fixed 4_3

--- bin/assingnment_4_3.dart
+++ bin/assingnment_4_3.dart
@@ -1,23 +1,27 @@
 import 'dart:io';
+import 'dart:math';
+import 'dart:convert';
 
 void main(List<String> arguments) {
-  List<String> InputList = [];
+  var InputList = [];
   bool condition = true;
 
   int largest_input = 0;
-  int smallest_input = 1;
+  int smallest_input = 0;
 
   while (condition) {
     String input = stdin.readLineSync();
     condition = input != '';
     List<int> InputListAsInt =
     InputList.map((data) => int.parse(data)).toList();
+    InputListAsInt.forEach((element) => smallest_input += element);
     if (input != '') {
       InputList.add(input);
     }
     else if (input == '') {
       {
         for (int b = 0; InputListAsInt.length > b; b++) {
+
           if (InputListAsInt[b] > largest_input) {
             largest_input = InputListAsInt[b];
           }
